f2200a13d76f1932ec6a0cb078301f339370503e
Samuel <sampaioxsamuel@gmail.com>
2021-01-02 16:11:40 -0300

    initial

---
+++ index.js
@@ -0,0 +1,6 @@
+const date = new Date("2021-01-07");
+
+function numberToDay(day) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[day]
+}
